f5bf7af46eb081df41d6071fd793f58895fef856
lhuisenga <98440150+lhuisenga@users.noreply.github.com>
2023-03-04 17:27:54 -0600

    pythonanywhere deployment setup

---
+++ locallibrary1/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.urls import include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
